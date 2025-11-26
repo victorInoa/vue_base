a0351bae427cd6496abe40f57a2beb2b064e5600
Victor inoa <victorinoa16@gmail.com>
2025-11-26 01:23:25 -0400

    feat(core) adding sweetalert2 and making form request example throw axio

--- src/views/examples/Form.vue
+++ src/views/examples/Form.vue
@@ -5,12 +5,37 @@ import InputPasswordBase from '@/components/base/forms/InputPasswordBase.vue'
 import ButtonBase from '@/components/base/forms/ButtonBase.vue'
 import { Icon } from '@iconify/vue'
 import CardBase from '@/components/base/CardBase.vue'
+import axios from 'axios'
+const handleSubmit = () => {
+  console.log('----submitting')
+  const formData = new FormData(event.target)
+  console.log(formData)
+  console.log(formData.get('user_name'))
+  console.log(formData.get('password'))
+  login(formData)
+}
+
+async function login(formData) {
+  try {
+    const response = await axios.post('http://localhost:8000', {
+      user_name: formData.get('user_name'),
+      password: formData.get('password'),
+      action: 'login',
+    })
+
+    if (response.statusText !== 'OK') {
+      throw new Error('Error al hacer login')
+    }
+  } catch (error) {
+    console.error('Error al hacerlo: ' + error.message)
+  }
+}
 </script>
 
 <template>
   <section-title>Como usar un formulario</section-title>
 
-  <form action="">
+  <form action="" @submit.prevent="handleSubmit">
     <CardBase>
       <template #header>
         <h2>Formulario de registro</h2>
@@ -20,7 +45,7 @@ import CardBase from '@/components/base/CardBase.vue'
         id="input_name"
         class="mb-3"
         label="Usuario"
-        name="name"
+        name="user_name"
         placeholder="Escribe tu nombre de usuario"
       />
 
@@ -32,7 +57,7 @@ import CardBase from '@/components/base/CardBase.vue'
         placeholder="Escribe tu contraseña"
       />
       <template #footer>
-        <ButtonBase variant="gradient">
+        <ButtonBase type="submit" variant="gradient">
           <Icon class="mr-2 inline" icon="fluent-color:send-clock-20" />
           Enviar
         </ButtonBase>
